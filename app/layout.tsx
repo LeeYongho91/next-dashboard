c8dcfcaeb469a1b8e0d656db71c83b24de663b3a
LeeYongho91 <skyks4848@naver.com>
2024-06-01 19:28:34 +0900

    로그인 메타 데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,25 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+
+
+
+  function add(a:number, b: number) {
+    return a+b;
+  }
+
+  let name = 10;
+
+  const foo = {
+    x: [1,2,3],
+    bar: {
+      name: 'test'
+    }
+  }
+
+const response = fetch("http://naver.com")
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
